3c1c0f164097a76a64c094b7b4a2ec1fdf02c8a3
MattRoger <mrogerphotography@gmail.com>
2019-10-20 13:38:28 -0600

    commiting before push

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require("express");
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-
+app.set("views engine", "pug")
 // Requiring our models for syncing
 var db = require("./models");
 
