a69e68d976ae3fcfe778b1569211d61d00e78de7
Nick Cooper <charliecoop@gmail.com>
2019-10-22 13:49:31 -0600

    Added login routes and pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,59 +5,81 @@ const pug = require('pug');
 var axios = require('axios');
 var db = require("../models")
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+    // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    //res.sendFile(path.join(__dirname, "../views/html/index.html"));
-    res.render('index', { user: req.user });
-  });
-  // routes added by matt
-  
-  app.get("/profilepage", function(req, res) {
-    res.render('profilepage', { user: req.user });
-  });
-  app.get("/searchresults", function(req, res) {
-    res.render('searchresults', { user: req.user });
-  });
-  app.get("/thespoils", function(req, res) {
-    res.render('thespoils', { user: req.user });
-  });
-  app.get("/newuser", function(req, res) {
-    res.render('newUser', { user: req.user });
-  });
+    // index route loads view.html
+    app.get("/", function (req, res) {
+        //res.sendFile(path.join(__dirname, "../views/html/index.html"));
+        res.render('index', { user: req.user });
+    });
+    // routes added by matt
 
-  app.post("/searchresults", function(req, res) {
-    console.log(req.body.movieSearch)
-    axios.get(`http://www.omdbapi.com/?apikey=79a1eb7f&s=${req.body.movieSearch}`).then(function(response){
-      var movies = response.data.Search;
-      res.render('searchresults', { movies: movies })
-    }).catch(function(err){
-      if(err) throw err
-    })
-    // res.render('searchresults', { user: req.user }).end();
-  });
+    app.get("/profilepage", function (req, res) {
+        res.render('profilepage', { user: req.user });
+    });
+    app.get("/searchresults", function (req, res) {
+        res.render('searchresults', { user: req.user });
+    });
+    app.get("/thespoils", function (req, res) {
+        res.render('thespoils', { user: req.user });
+    });
+    app.get("/newuser", function (req, res) {
+        res.render('newUser', { user: req.user });
+    });
 
-  app.get("/thespoils/:id", function(req, res) {
-    axios.get(`http://www.omdbapi.com/?apikey=79a1eb7f&i=${req.params.id}`).then(function(response){
-      console.log(response.data)
-      var movieInfo = response.data;
-      db.Movie.findOrCreate({
-        where: {
-          name: movieInfo.Title
-        }, defaults:{ 
-          name: movieInfo.Title,
-          image_path: "",
-        }}).spread(function(movie, created){
-          return res.render('thespoils', { spoilsInfo: movie, movieInfo: movieInfo })
-          console.log(created)
+    // routes added by Nick
+    app.get("/register", function (req, res) {
+        res.render('register');
+    });
+
+    app.get("/login", function (req, res) {
+        res.render('login');
+    });
+
+    app.get("/profile", function (req, res) {
+        if (!req.user) {
+            return res.redirect('/login');
+        }
+        res.render('profilepage', { username: req.user.username });
+    });
+
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.post("/searchresults", function (req, res) {
+        console.log(req.body.movieSearch)
+        axios.get(`http://www.omdbapi.com/?apikey=79a1eb7f&s=${req.body.movieSearch}`).then(function (response) {
+            var movies = response.data.Search;
+            res.render('searchresults', { movies: movies })
+        }).catch(function (err) {
+            if (err) throw err
         })
-    }).catch(function(err){
-      if(err) throw err
-    })
-    // res.render(200).end();
-  });
+        // res.render('searchresults', { user: req.user }).end();
+    });
+
+    app.get("/thespoils/:id", function (req, res) {
+        axios.get(`http://www.omdbapi.com/?apikey=79a1eb7f&i=${req.params.id}`).then(function (response) {
+            console.log(response.data)
+            var movieInfo = response.data;
+            db.Movie.findOrCreate({
+                where: {
+                    name: movieInfo.Title
+                }, defaults: {
+                    name: movieInfo.Title,
+                    image_path: "",
+                }
+            }).spread(function (movie, created) {
+                return res.render('thespoils', { spoilsInfo: movie, movieInfo: movieInfo })
+                console.log(created)
+            })
+        }).catch(function (err) {
+            if (err) throw err
+        })
+        // res.render(200).end();
+    });
 
 };
